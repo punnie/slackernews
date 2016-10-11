bb5b0ec445d3c61d3e4c843c0f97c13fb65a806f
Pedro Coelho <pedro.luis.coelho@gmail.com>
2016-10-11 10:24:16 -0700

    Change user-agent

--- src/slackernews/scrapper.clj
+++ src/slackernews/scrapper.clj
@@ -9,7 +9,7 @@
             [clojure.tools.logging :as log]))
 
 (def user-agent
-  {"User-Agent" "facebookexternalhit/1.1"})
+  {"User-Agent" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50"})
 
 (defstate scrapped-channels
   :start (clojure.string/split (env :scrapped-channels) #","))
