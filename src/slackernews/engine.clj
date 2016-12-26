8f5c3a6078db1201e7b923678c6f7e4271c8b85e
Pedro Coelho <pedro.luis.coelho@gmail.com>
2016-12-26 21:03:31 +0000

    Rename engine's functions for better understanding

--- src/slackernews/engine.clj
+++ src/slackernews/engine.clj
@@ -6,28 +6,7 @@
                                         <!]]
             [clojure.tools.logging :as log]))
 
-; {:id "060A6EEC-4C66-4FFC-AF55-F48D9BC0A2E6"
-;  :connection {:url "https://slack.com/api/" :token "xoxb-94923848867-dgXkpREpB9jnSCMkaIh0xJOM"}}
-; (condp = type
-;         "hello"           (let [timestamp (System/currentTimeMillis)]
-;                             (log/info "Connected to slack at" timestamp))
-;         "pong"            (let [timestamp (System/currentTimeMillis)]
-;                             (log/info "Last pong received at" timestamp))
-;         "user_change"     (let [user          (:user slack-event)
-;                                 user-slack-id (:id user)]
-;                             (log/info "Updating user information for user" user-slack-id)
-;                             (db/update-user user-slack-id user))
-;         "presence_change" (let [presence      (:presence slack-event)
-;                                 user-slack-id (:user slack-event)]
-;                             (log/info "Updating user presence for user" user-slack-id)
-;                             (db/update-user-presence user-slack-id presence))
-;         "channel_created" (let [channel          (:channel slack-event)
-;                                 channel-slack-id (:id channel)]
-;                             (log/info "Creating new channel" channel-slack-id)
-;                             (db/create-channel channel))
-;         (log/info slack-event))
-
-(defn client-loop
+(defn team-loop
   ""
   [{:keys [team out-stream in-stream] :as context}]
   (go-loop []
@@ -37,22 +16,22 @@
         (log/info "Unimplemented event:" slack-event))
       (recur))))
 
-(defn client-connect
+(defn team-connect
   ""
-  [{:keys [connection] :as client}]
-  (when-let [{:keys [team
-                     channels
-                     groups
-                     users
-                     bots
+  [{:keys [connection] :as team}]
+  (when-let [{:keys [slack-team
+                     slack-channels
+                     slack-groups
+                     slack-users
+                     slack-bots
                      in-stream
                      out-stream]} (ingress/slack-connect connection)]
-    (do (db/update-team client team)
-        (log/debug "User count" (count users))
-        (log/debug "Bot count" (count bots))
-        (doall (pmap (partial db/upsert-user client) users))
-        (doall (pmap (partial db/upsert-user client) bots))
-        (doall (pmap (partial db/upsert-channel client) channels))
-        (doall (pmap (partial db/upsert-channel client) groups)))
-    (client-loop {:team (assoc client :slack team) :out-stream out-stream :in-stream in-stream})
+    (do (db/update-team connection team)
+        (log/debug "User count" (count slack-users))
+        (log/debug "Bot count" (count slack-bots))
+        (doall (pmap (partial db/update-user client) slack-users))
+        (doall (pmap (partial db/update-user client) slack-bots))
+        (doall (pmap (partial db/update-channel client) slack-channels))
+        (doall (pmap (partial db/update-channel client) slack-groups)))
+    (team-loop {:team (assoc client :slack team) :out-stream out-stream :in-stream in-stream})
     out-stream))
