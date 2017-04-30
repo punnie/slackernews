ef94bb1c8bb100b915aba4734013daf8a1d9bce0
Pedro Coelho <punnie@ethereal.io>
2017-04-30 02:26:54 +0100

    Add rudimentary fetch-links function and views

--- src/clj/slackernews/controllers/links.clj
+++ src/clj/slackernews/controllers/links.clj
@@ -1,7 +1,23 @@
 (ns slackernews.controllers.links
-  (:require [slackernews.controllers.layout.html :as layout]))
+  (:require [slackernews.controllers.layout.html :as layout]
+            [slackernews.db.link :as ldb]))
+
+(defn- display-links
+  ""
+  [team]
+  (let [links (ldb/fetch-links team)]
+    [:ul (for [link links]
+           [:li (str link)])]))
+
+(defn- display-add-to-slack
+  ""
+  []
+  [:p (str "Add to slack!")])
 
 (defn display
   ""
-  [{:keys [session params] :as request}]
-  (layout/default [:p (str "Hello " (str (or (:team request) "nil")))]))
+  [{:keys [session params team] :as request}]
+  (layout/default
+   (if team
+     (display-links team)
+     (display-add-to-slack))))
--- src/clj/slackernews/db/link.clj
+++ src/clj/slackernews/db/link.clj
@@ -1,5 +1,6 @@
 (ns slackernews.db.link
   (:require [monger.collection :as mc]
+            [monger.query :as mq]
             [slackernews.db.core :refer [db]]))
 
 (def collection "" "links")
@@ -11,3 +12,11 @@
                {:ts ts :team_id team_id :channel_id channel_id :user_id user_id}
                link
                {:upsert true}))
+
+(defn fetch-links
+  ""
+  [{:keys [team_id] :as team} & {:keys [page per-page] :or {page 1 per-page 20}}]
+  (mq/with-collection db collection
+    (mq/find {:team_id team_id})
+    (mq/sort (sorted-map :ts -1))
+    (mq/paginate :page page :per-page per-page)))
