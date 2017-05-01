cda75b6c76bad395807cf221a38e669033be0ee9
Pedro Coelho <punnie@ethereal.io>
2017-05-01 02:38:02 +0100

    Implement rudimentary meta tags scraper

--- src/clj/slackernews/processor/scraper.clj
+++ src/clj/slackernews/processor/scraper.clj
@@ -1,12 +1,11 @@
 (ns slackernews.processor.scraper
-  (:require [clojure.java.io :as io]
+  (:require [clojure.string :as string]
             [http.async.client :as http]
             [http.async.client.request :as request]
+            [net.cgrand.enlive-html :as html]
             [pantomime.mime :as mime]
             [taoensso.timbre :as log])
-  (:import [java.net UnknownHostException
-                     NoRouteToHostException]
-           [javax.net.ssl SSLProtocolException]))
+  (:import (java.io StringReader)))
 
 (def user-agent "" (str "slackernews/"
                         (System/getProperty "slackernews.version")
@@ -35,13 +34,47 @@
 (defn get-mime-type
   ""
   [{:keys [response] :as link-info}]
-  (let [body (.toByteArray @(:body response))]
+  (let [body (.toByteArray (http/body response))]
     (assoc-in link-info [:mime-type] (mime/mime-type-of body))))
+
+(defn get-meta-tags
+  ""
+  [content]
+  (let [title       (-> (html/select content [:head :title])
+                        first
+                        :content
+                        first)
+        description (-> (html/select content [:head [:meta (html/attr= :name "description")]])
+                        first
+                        :attrs
+                        :content)
+        og          (for [tag (html/select content [:head [:meta (html/attr-starts :property "og:")]])]
+                      (let [tag-attrs (-> tag :attrs)
+                            key       (keyword (clojure.string/replace (:property tag-attrs) (re-pattern "og:") ""))
+                            value     (:content tag-attrs)]
+                        {key value}))]
+    {:meta {:title title :description description}
+     :og   (into {} og)}))
+
+(defn get-meta-information
+  ""
+  [{:keys [response] :as link-info}]
+  (if-let [meta-info (case (:mime-type link-info)
+                       ("text/html" "application/xhtml+xml") (-> (http/body response)
+                                                                 (.toString)
+                                                                 (StringReader.)
+                                                                 (html/html-resource)
+                                                                 (get-meta-tags))
+                       nil)]
+    (assoc-in link-info [:meta] meta-info)
+    link-info))
 
 (defn get-link-information
   ""
   [{:keys [url] :as link-info}]
+  (log/info url)
   (-> link-info
       (request-uri)
       (get-mime-type)
-      (get-in [:response :url])))
+      (get-meta-information)
+      :meta))
